0aed49009fa79df8f79b482fb6fe6d98963617e3
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-14 12:06:26 -0500

    Makes print statements to test parser

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
-if __name__ == '__main__':
-    main()
+import Parser
 
 def main():
     while True:
-        print('>>>')
+        verb, noun, indirect, objection = Parser.parse(input('>>>'))
+        if objection == 'none':
+            print(verb, noun, indirect)
+        else:
+            print(objection)
+
+if __name__ == '__main__':
+    main()
