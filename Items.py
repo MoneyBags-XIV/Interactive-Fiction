8fe055ec8b7dd5db80b92f4bec23f27e0059bf33
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-13 19:39:03 -0500

    Fixes initialization error of Item instances

---
+++ Items.py
@@ -0,0 +1,6 @@
+import Definitions
+
+items = [
+    Definitions.Item(['thing', 'object'], 'This is a boring thing.', True),
+    Definitions.Item(['refrigerator', 'fridge'], 'This is an ordinary, white refrigerator.', False, portable=False, container=True)
+]
