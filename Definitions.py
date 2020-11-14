0c9c41cba18bdb4996386c70ac0488c3888a6fe2
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-13 19:38:07 -0500

    Defines str method for class Item

--- Definitions.py
+++ Definitions.py
@@ -1,8 +1,14 @@
 class Item:
-    def __init__(self, names, breakable, deadly, portable=True, edible=False, drinkable=False,):
+    def __init__(self, names, description, breakable, deadly=False, portable=True, edible=False, drinkable=False, container=False):
         self.names = names
+        self.description = description
         self.breakable = breakable
         self.deadly = deadly
         self.portable = portable
         self.edible = edible
         self.drinkable = drinkable
+        self.container = container
+
+    def __str__(self):
+        return self.names
+
