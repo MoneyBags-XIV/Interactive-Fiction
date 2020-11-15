a64696ba196bb84ad9376c6010dd526d2f6b1f8a
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-15 13:11:37 -0500

    Adds feature to call verb function from main

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import Parser
+import Verbs
 
 def main():
     while True:
         verb, noun, indirect, objection = Parser.parse(input('>>>'))
         if objection == 'none':
             print('verb: '+verb, '\nnoun: '+noun, '\nindirect: '+indirect)
+            Verbs.verbs[verb]()
         else:
             print(objection)
 
