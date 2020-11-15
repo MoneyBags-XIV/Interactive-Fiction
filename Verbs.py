10c3b42623244cfce633af584be86aa54963d7ca
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-15 13:11:04 -0500

    Makes verb functions and matches them to a string in a dictionary

--- Verbs.py
+++ Verbs.py
@@ -1,1 +1,16 @@
-verbs = ['hit', 'take']
+def hit():
+    print(':)')
+
+def take():
+    print(':P')
+
+def go():
+    print(':(')
+
+verbs = {'hit': hit,
+    'kill': hit,
+    'smack': hit,
+    'kick': hit,
+    'take': take,
+    'go': go
+}
