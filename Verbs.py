30a7ccf412b56f057b40e0a2a67762a473690970
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-16 20:50:23 -0500

    Adds function to return unknown noun

--- Verbs.py
+++ Verbs.py
@@ -13,6 +13,10 @@ def find_indirect(verb, noun):
     indirect = input('What do you want to '+verb+' the '+noun+' with?\n>>>')
     return indirect
 
+def find_noun(verb):
+    noun = input('What do you want to '+verb+'?')
+    return noun
+
 def hit(noun, indirect):
     print('Killed.')
 
