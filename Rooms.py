d3cbe94f1f5e795599e0272ed1ec39a015928445
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-17 18:54:00 -0500

    Adds first room definitions

--- Rooms.py
+++ Rooms.py
@@ -4,10 +4,16 @@ class Room:
         self.name = name
         self.description = description
         self.directions = directions
-        
+
     def move(self, direction):
         if direction in self.directions.keys():
             return self.directions[direction]
         
         else:
             return False
+
+rooms = [
+    Room(0, 'Inventory', 'This is the place where you carry things. Although, you can\t carry that many things since it\'s been a while since you\'ve been to the gym.'),
+    Room(1, 'Room', 'This is a boring, ordinary room.', N=2),
+    Room(2, 'Bigger Room', 'This room is just as boring, but it\'s a bit bigger.', S=1)
+]
