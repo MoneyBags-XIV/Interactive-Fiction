73f56ee01b30533f69bf3e8812149c3d35679e2d
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-17 18:43:44 -0500

    Adds new parser snark for unknown word

--- Parser.py
+++ Parser.py
@@ -1,5 +1,6 @@
 import Items
 import Verbs
+from random import choice
 
 def parse(input):
     case = input.lower()
@@ -15,7 +16,7 @@ def parse(input):
 
     for word in words:
         if word not in Verbs.verbs.keys() and Items.check_items(word) == False and word != 'using' and word != 'use' and word != 'with':
-            objection = 'I don\'t know the word "' + word + '".'
+            objection = choice(['I don\'t know the word "' + word + '".', 'You can play this entire game, find every hidden secret, and get every posible point, without once using the word: "'+word+'".'])
     
     if objection == 'none':
         for word in words:
