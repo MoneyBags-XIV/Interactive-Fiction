da65678d808ce806a3e25809ee07ecf0cf2b2b96
Isaac Lenzini <isaac.lenzini@gmail.com>
2020-11-16 20:18:42 -0500

    adds snarky respose for noun == indirect

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
 import Parser
 import Verbs
+from random import choice
 
 def main():
     while True:
         verb, noun, indirect, objection = Parser.parse(input('>>>'))
         if objection == 'none':
-            print('verb: '+verb, '\nnoun: '+noun, '\nindirect: '+indirect)
-            Verbs.verbs[verb]()
+            if noun == indirect:
+                print(choice(Verbs.generic_sass))
+            else:
+                Verbs.verbs[verb]()
         else:
             print(objection)
 
